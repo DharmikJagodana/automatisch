301dd0841210acd2026221cfd7de62eadbed1c22
Rıdvan Akca <akca.ridvann@gmail.com>
2024-01-10 12:47:18 +0300

    feat(vtiger-crm): add new opportunities trigger

--- packages/backend/src/apps/vtiger-crm/triggers/index.ts
+++ packages/backend/src/apps/vtiger-crm/triggers/index.ts
@@ -2,5 +2,6 @@ import newCases from './new-cases';
 import newContacts from './new-contacts';
 import newInvoices from './new-invoices';
 import newLeads from './new-leads';
+import newOpportunities from './new-opportunities';
 
-export default [newCases, newContacts, newInvoices, newLeads];
+export default [newCases, newContacts, newInvoices, newLeads, newOpportunities];
