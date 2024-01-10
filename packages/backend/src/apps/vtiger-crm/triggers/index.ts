0b9cd00203f4ce26a6b17eead8905efdb9db16bf
Rıdvan Akca <akca.ridvann@gmail.com>
2024-01-10 14:32:09 +0300

    feat(vtiger-crm): add new todos trigger

--- packages/backend/src/apps/vtiger-crm/triggers/index.ts
+++ packages/backend/src/apps/vtiger-crm/triggers/index.ts
@@ -3,5 +3,13 @@ import newContacts from './new-contacts';
 import newInvoices from './new-invoices';
 import newLeads from './new-leads';
 import newOpportunities from './new-opportunities';
+import newTodos from './new-todos';
 
-export default [newCases, newContacts, newInvoices, newLeads, newOpportunities];
+export default [
+  newCases,
+  newContacts,
+  newInvoices,
+  newLeads,
+  newOpportunities,
+  newTodos,
+];
